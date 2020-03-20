765a5ed54b6dff2cea8aa5ac3dac9f62b2092873
koho <gerhard.gh.ta@gmail.com>
2020-03-20 15:07:48 +0800

    Update wqy.py

--- mpl_font/wqy.py
+++ mpl_font/wqy.py
@@ -4,4 +4,7 @@ from mpl_font import apply_font
 
 FONT_NAME = 'WenQuanYi Micro Hei'
 
-apply_font(os.path.join(os.path.dirname(__file__), 'fonts', 'wqy'), FONT_NAME)
+def set():
+  apply_font(os.path.join(os.path.dirname(__file__), 'fonts', 'wqy'), FONT_NAME)
+  
+set()
