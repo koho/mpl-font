281b26961f21189de9d76bffdaa3d3af4cd61d1e
koho <gerhard.gh.ta@gmail.com>
2020-03-20 15:07:23 +0800

    Update noto.py

--- mpl_font/noto.py
+++ mpl_font/noto.py
@@ -4,4 +4,7 @@ from mpl_font import apply_font
 
 FONT_NAME = 'Noto Sans CJK SC'
 
-apply_font(os.path.join(os.path.dirname(__file__), 'fonts', 'noto'), FONT_NAME)
+def set():
+  apply_font(os.path.join(os.path.dirname(__file__), 'fonts', 'noto'), FONT_NAME)
+
+set()
